dc92cedf1256f8b8ba4a840b82db16e7133b8861
Trelana Martin <tremartin3003@gmail.com>
2023-12-17 01:43:07 +0000

    Followed instructions in Studies project setup video to get started

---
+++ Studies/data-types.js
@@ -0,0 +1,2 @@
+// In the setup vid Teach says name the files just like his ("datatypes") but he also said use a hyphen for docs with spaces.  
+// Is it ok for me to leave the hyphen in this doc name? 
---
+++ Studies/variables.js
@@ -0,0 +1,19 @@
+// Important Notes: 1) I hear a kitty cat in the setup vid.  2) We have to show a minimum of 4 srting methods.
+
+/** 
+ * VARIABLES:
+ * 
+ * 0: 5 sentence summary of essential variable facts. 
+ * 
+ * 1: Declaration & Assignment: To create a variable we must 1assign it using... etc.  Explain the process. 
+ * 
+ * 2: Var, let, const are the 3 declarative keywords: Explain them as well. 
+ * 
+ * 3: Hoisting: (describe as well)
+ * 
+ * 
+ */
+
+// 1. Declaration & Assignment //
+
+// ((Code examples here with pseudocoding))  Refer to the presentation for examples.
