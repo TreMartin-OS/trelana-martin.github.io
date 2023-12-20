3a6b8358cd6b0e4db85765b2da614f30646d3d9a
Trelana Martin <tremartin3003@gmail.com>
2023-12-20 03:54:17 +0000

    Saving finished functions.js homework

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -15,7 +15,7 @@
 
 
 // Declare Function named search that takes 2 parameters: an array & string
-function search(arr, str) {
+function search(animals, str) {
 
 
 ///////////////////// Solution 1
@@ -23,15 +23,15 @@ function search(arr, str) {
     let answer;
     
       // Loop thru the array
-      for (var i = 0; i < arr.length; i++) {
+      for (var i = 0; i < animals.length; i++) {
           // Check if the name value of any objects matches the string input
-          if (arr[i].name === str) {
+          if (animals[i].name === str) {
               // If it does, return the entire Object
-              answer = arr[i];
+              answer = animals[i];
               //break if found or the loop will run to the end of the array
-            break;
+                break;
           } // If none do, return null
-          else if (arr[i].name !== str) {
+          else if (animals[i].name !== str) {
               answer = null;
           } // end if-else statements
       } // end loop
@@ -45,13 +45,13 @@ function search(arr, str) {
 //////////////////////////////////////////////////////////////////////
 
 // declare a replace function like this: replace(animals array, name string, replacement object)
-function replace(arr, str, obj) {
+function replace(animals, name, replacement) {
     // Loop thru the array
-    for (var i = 0; i < arr.length; i++)
+    for (var i = 0; i < animals.length; i++)
         // Check If an animal with the string name exists within the `animals` Array,  
-        if (arr[i].name === str) {
+        if (animals[i].name === name) {
             // If so, replace it's entire Object with the replacement Object.
-            arr.splice(arr[i], 1, obj);
+            animals.splice(animals[i], 1, replacement);
         }
 }
 
@@ -61,11 +61,49 @@ function replace(arr, str, obj) {
 //////////////////////////////////////////////////////////////////////
 
 
+// Declare a function that matches: remove(animals array, name string)
+function remove(animals, name) {
+    // Loop Thru array
+    for (var i = 0; i < animals.length; i++) {
+        // Check If an animal with that name exists within the Array 
+        if (animals[i].name === name) { // SpecRuner says it cant read name here, but also says it does elsewhere?
+            console.log(animals[i]);
+            //  If so, remove it.
+            animals.splice([i], 1);
+        }
+    }
+    
+}
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// Declare a function with `add(animals array, animal obj)
+function add(animals, animal) {
+
+// storage array
+var storage = [];
+
+// Loop thru array
+for (var i = 0; i < animals.length; i++) {
+    // check if other animals have the same name
+    if (animals[i].name === animal.name) {
+        // If they do, push to storage array
+        storage.push(animals[i]);
+    }
+}
+// If storage array doesn't have a length
+if (storage.length === 0) {
+    // then Check if the >Object< has a `name` property with a length > 0 & a `species` property with a length > 0
+    if (animal.name.length > 0 && animal.species.length > 0) {
+// If so, add the new Object to the `animals` Array
+animals.push(animal);
+    }
+}
+}
 
 
 /**
